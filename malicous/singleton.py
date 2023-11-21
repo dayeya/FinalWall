728a3e0845e259e838da6a7b175f98e7e5607b29
Daniel <129618322+dayeya@users.noreply.github.com>
2023-11-22 00:14:01 +0200

    Update singleton.py

--- malicous/singleton.py
+++ malicous/singleton.py
@@ -1,7 +1,6 @@
 class Singleton(type):
     __instances = {}
     
-    # function to fetch the singleton object.
     def __call__(cls, *args, **kwargs):
         """
         Returns the instances if it exists, otherwise creates it.
