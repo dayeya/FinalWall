d2e775b58aabfa69fa98ce7cd84f29e45a87063f
dayeya <clipchamp.daniel@gmail.com>
2023-11-28 10:21:30 +0200

    Better structure of code.

--- util_modules/network.py
+++ util_modules/network.py
@@ -7,7 +7,7 @@ Short function should be documented like this.
 
 import sys
 from socket import *
-from typing import Callable
+from typing import Callable, Union
 from threading import Thread
 from network_client import Client
 from conversion import from_bin, from_hex, decode, encode
@@ -17,16 +17,29 @@ all_interfaces = "0.0.0.0"
 buffer_size = 4096
 listen_bound = 5
 
+Operation_Result = Union[bytes, None]
+
+def __safe_socket_operation(func: Callable, sock: socket, *args: tuple) -> Operation_Result:
+    """
+    Wrapper function to perform any socket operation.
+    :params: func - Callable, sock - socket, args - tuple.
+    :return: Result of the operation.
+    """
+    try:
+        return func(sock, *args)
+    except Exception as e:
+        print(f"Error while: {func.__name__} on socket: {sock.getsockname()}, {e}")
+
 def recv(sock: socket) -> str:
     """
     Waits for data from sock.
     :params: sock - socket.
     :return: decoded data.
     """
-    try:
-        return decode(sock.recv(buffer_size))
-    except Exception as e:
-        print(f'[!] {e} Could not recieve any data from: {sock.getsockname()}')
+    def nest_recv(sock: socket, buffer: int) -> bytes:
+        return sock.recv(buffer)
+    
+    return decode(__safe_socket_operation(nest_recv, sock, buffer_size))
 
 def send(sock: socket, payload: str) -> None:
     """
@@ -34,14 +47,14 @@ def send(sock: socket, payload: str) -> None:
     :params: sock - socket, payload - str.
     :return: None.
     """
-    try:
-        sock.send(encode(payload))
-    except Exception as e:
-        print(f'[!] {e} Could not send any data to: {sock.getpeername()}')
+    def nest_send(sock: socket, payload: str) -> bytes:
+        return sock.send(payload)
+    
+    __safe_socket_operation(nest_send, sock, payload)
 
 def send_recv(sock: socket, payload: str) -> str:
     """
-    Sends a payload from sock and waits for an answer.
+    Sends a payload from sock and waits for an answer, using a safe operation.
     :params: sock - socket, payload - str.
     :return: Decoded answer.
     """
