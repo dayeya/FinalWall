bb7bc57cdf299b11c067b2fd0da5b2193eea0ac3
dayeya <clipchamp.daniel@gmail.com>
2023-12-05 13:04:13 +0200

    http request parser

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Picky - Web application firewall.
 
 My Final YB Project.
+
+## Contributions
+This project was writting only by Daniel, as opposed to the contributors list.
--- reverse_proxy/http_handling/protocol.py
+++ reverse_proxy/http_handling/protocol.py
@@ -23,6 +23,15 @@ class HTTPRequest(BaseHTTPRequestHandler):
     """
     Basic Wrapper to a http request for parsing and using raw bytes.
     """
+    def __init__(self, raw_packet: bytes):
+        """
+        Http request parser.
+        """
+        self._chunk = raw_packet
+        self.__bytes_file = BytesSocket(self._chunk)
+
+        super().__init__(self.__bytes_file)
+        self.parse_request()
         
 class HTTPServerResponse(HTTPResponse):
     """
@@ -30,7 +39,7 @@ class HTTPServerResponse(HTTPResponse):
     """
     def __init__(self, packet: bytes) -> None:
         """
-        configuration of HTTPServerResponse.
+        Http response parser.
         """
         self._chunk = packet.split(b'\r\n\r\n', 1)[0]
         self.__bytes_file = BytesSocket(self._chunk)
