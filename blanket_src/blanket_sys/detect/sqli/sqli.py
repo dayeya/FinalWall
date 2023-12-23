42dc555c6bff86e90ce278911b30d99cc93efebd
dayeya <clipchamp.daniel@gmail.com>
2023-12-23 17:51:59 +0200

    Added class SqlPayload, SqlDetectionResult

--- blanket_src/blanket_sys/detect/sqli/sqli.py
+++ blanket_src/blanket_sys/detect/sqli/sqli.py
@@ -1,5 +1,30 @@
 from typing import Union
-import urllib.parse
+from dataclasses import dataclass
+from urllib.parse import unquote, parse_qs
 
-def sqli(payload: str) -> None: 
+@dataclass
+class SqlDetectionResult:
+    result: bool
+    description: str
+
+@dataclass
+class SqlPayload:
+    payload: str
+    identifier: str
+
+    def parse(self) -> None:
+        self.payload = unquote(self.payload)
+        
+    def len(self):
+        return len(self.payload)
+    
+def with_quotes(s: SqlPayload) -> bool:
     pass
+
+def with_keywords(s: SqlPayload) -> bool:
+    pass
+
+def sqli(s: SqlPayload) -> SqlDetectionResult: 
+    if s.len() == 0:
+        return SqlDetectionResult(True, 'Empty payload')
+    
--- blanket_src/http_tools/functions.py
+++ blanket_src/http_tools/functions.py
@@ -4,10 +4,8 @@ http functions module.
 """
 from .protocol import HTTPRequestParser
 
-END_SUFFIX = b'\r\n\r\n'
-    
 def has_ending_suffix(payload: bytes) -> bool:
-    return END_SUFFIX in payload
+    return b'\r\n\r\n' in payload
 
 def get_content_length(payload: HTTPRequestParser, default: int=-1) -> int:
     content_length = payload.getheader('Content-Length', default)
