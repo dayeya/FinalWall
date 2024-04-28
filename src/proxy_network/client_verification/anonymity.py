55fae18cbc9e173f42cb2cf879697afcedbca905
Daniel <clipchamp.daniel@gmail.com>
2024-04-28 17:26:01 +0300

    Maxmind API addition and DB.

--- src/proxy_network/client_verification/anonymity.py
+++ src/proxy_network/client_verification/anonymity.py
@@ -1,6 +1,12 @@
-from src.proxy_network.client_verification.acl import AccessList
+import geoip2.database
+from pathlib import Path
+
 from src.net.aionetwork import HostAddress
 from src.internal.system.transaction import Transaction
+from src.proxy_network.client_verification.acl import AccessList
+
+ROOT_DIR = Path(__file__).parent.parent
+__mmdb_path = ROOT_DIR / "geoip2_db" / "GeoLite2-Country.mmdb"
 
 
 def validate_anonymity_from_proxies(tx: Transaction, access_list: AccessList) -> bool:
@@ -29,7 +35,8 @@ def validate_hosts_geoip(host: HostAddress):
     :param host:
     :return:
     """
-    pass
+    with geoip2.database.Reader(__mmdb_path) as reader:
+        pass
 
 
 def validate_dirty_client(host: HostAddress, access_list: AccessList) -> bool:
